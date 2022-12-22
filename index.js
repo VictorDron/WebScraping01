3f509c7c341968111e565203197f5253e9b9e260
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-12-22 18:48:01 -0300

    ok

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const puppeteer = require('puppeteer');
   
     await page.goto('https://www.google.com.br/maps/place/Campo+Grande,+Rio+de+Janeiro+-+RJ/@-22.8877585,-43.5487871,12z/data=!3m1!4b1!4m5!3m4!1s0x9be17999363715:0x46c3f27867ad9332!8m2!3d-22.9076515!4d-43.5659121?hl=pt-BR&authuser=0');
   
-    await page.click('[class="g88MCb S9kvJb"]');
+    await page.click('[data-value="Próximo"]');
+
+    await page.type('[class="searchboxinput xiQnY"]', 'Escolas particulares');
+
+    await page.click('[class="mL3xi"]');
 
     
 
