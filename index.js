a8c33f982ff004742e2197941e0c421bfc556beb
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-12-24 13:23:55 -0300

    ok

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const puppeteer = require('puppeteer');
 
+const CampoGrande = "Campo+Grande";
+const Escolas = "escolas+particulares";
+const Mapa = "https://www.google.com.br/maps/search/Escolas/@-22.9042277,-43.5996659,13z/data=!4m7!2m6!3m5!2sCampoGrande,+Rio+de+Janeiro+-+RJ!3s0x9be17999363715:0x46c3f27867ad9332!4m2!1d-43.5659121!2d-22.9076515?hl=pt-BR&authuser=0";
 
 
 (async () => {
@@ -7,26 +10,20 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
   
-    await page.goto('https://www.google.com.br/maps/place/Campo+Grande,+Rio+de+Janeiro+-+RJ/@-22.8877585,-43.5487871,12z/data=!3m1!4b1!4m5!3m4!1s0x9be17999363715:0x46c3f27867ad9332!8m2!3d-22.9076515!4d-43.5659121?hl=pt-BR&authuser=0');
+    await page.goto(Mapa);
   
-    await page.click('[data-value="Próximo"]');
-
-    await page.type('[class="searchboxinput xiQnY"]', 'Escolas particulares');
-
-    await page.click('[class="mL3xi"]');
-
       const leads = await page.evaluate (()=>{
 
-              const NodeList = document.querySelectorAll('[class="m6QErb tLjsW"]');
+              const NodeList = document.querySelectorAll('[class="y7PRA"]');
 
               const ListArray = [...NodeList];
 
-              //const list = ListArray.map( ({text}) => ({
-                  
-              //}));
+              const list = ListArray.map( ({src}) => ({
+                  src
+              }));
 
-            console.log(ListArray);
-          return leads;
+            console.log(list);
+          
       });
 
     
