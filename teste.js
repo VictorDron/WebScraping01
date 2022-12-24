a8380920359e69e0a7dac7be777d1599ca19e0c3
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2022-12-23 23:21:25 -0300

    ok

--- teste.js
+++ teste.js
@@ -5,17 +5,18 @@ const Escolas = "escolas+particulares";
 const Mapa = "https://www.google.com.br/maps/search/Escolas/@-22.9042277,-43.5996659,13z/data=!4m7!2m6!3m5!2sCampoGrande,+Rio+de+Janeiro+-+RJ!3s0x9be17999363715:0x46c3f27867ad9332!4m2!1d-43.5659121!2d-22.9076515?hl=pt-BR&authuser=0";
 
 
-async function autoSccroll(){
+async function autoScroll(page){
 
   await page.evaluate(async ()=>{
 
      await new Promise((resolve,reject)=>{
         
       var totalHeight = 0;
-      var distance = 100;
+      var distance = 300;
       var timer = setInterval(()=>{
            const element = document.querySelectorAll('[class:"section-scrollbox"]');
-           window.scrollBy(0,distance);
+           var scrollHeight = element.scrollHeight;
+           element.scrollBy(0,distance);
            totalHeight += distance;
            if(totalHeight >= scrollHeight){
                clearInterval(timer);
@@ -50,11 +51,14 @@ async function parsePlaces (page) {
 
 (async () => {
 
-  const browser = await puppeteer.launch({ headless: true});
+  const browser = await puppeteer.launch({ headless: false});
   const page = await browser.newPage();
   await page.goto(Mapa);
 
   const places = await parsePlaces(page);
+
+  await autoScroll(page);
+
   console.log(places);
 
   await page.setViewport({
