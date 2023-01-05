84558964ca4efeb372b61e5e786f562434ec9067
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2023-01-05 19:36:19 -0300

    ok

--- scroll2.js
+++ scroll2.js
@@ -7,13 +7,20 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({ headless: false});
     const [page] = await browser.pages();
 
-    await page.goto('https://www.youtube.com/');
-    await page.waitForSelector('.style-scope ytd-app');
+    await page.goto('https://www.google.com.br/maps/search/Restaurantes/@-22.9054608,-43.5686931,16z/data=!4m8!2m7!3m6!1sRestaurantes!2sCampo+Grande,+Rio+de+Janeiro+-+RJ!3s0x9be17999363715:0x46c3f27867ad9332!4m2!1d-43.5659121!2d-22.9076515?hl=pt-BR&authuser=0');
+    //await page.waitForSelector('');
 
-    const data = await page.evaluate(() => {
-      const container = document.querySelector('.style-scope ytd-app');
-      container.scrollBy(0, container.clientHeight);
-    });
+    // await page.waitForNavigation();
+    const scrollable_section = '.m6QErb DxyBCb kA9KIf dS8AEf ecceSd QjC7t';
+
+    await page.waitForSelector('.m6QErb DxyBCb kA9KIf dS8AEf ecceSd QjC7t');
+    
+    await page.evaluate(selector => {
+      const scrollableSection = document.querySelector(selector);
+    
+      scrollableSection.scrollDown = scrollableSection.offsetHeight;
+    }, scrollable_section);
+    
 
     await page.setViewport({
         width: 1900,
--- web5.js
+++ web5.js
@@ -5,14 +5,16 @@ const puppeteer = require('puppeteer');
         headless: false
     });
     const page = await browser.newPage();
-    await page.goto('https://www.youtube.com/');
+    await page.goto('https://www.google.com.br/maps/search/Restaurantes/@-22.9054608,-43.5686931,16z/data=!4m8!2m7!3m6!1sRestaurantes!2sCampo+Grande,+Rio+de+Janeiro+-+RJ!3s0x9be17999363715:0x46c3f27867ad9332!4m2!1d-43.5659121!2d-22.9076515?hl=pt-BR&authuser=0');
 
-    await page.focus('.style-scope yt-img-shadow');
-
+    
     await page.setViewport({
         width: 1900,
         height: 1300
     });
+    
+    //await page.waitForSelector('.m6QErb DxyBCb kA9KIf dS8AEf ecceSd QjC7t');
+    await page.focus('.m6QErb DxyBCb kA9KIf dS8AEf ecceSd QjC7t');
 
     await autoScroll(page);
 
@@ -29,12 +31,12 @@ async function autoScroll(page){
     while (true) {
 
         await page.evaluate(async () => {
+             page.focus('.m6QErb DxyBCb kA9KIf dS8AEf ecceSd QjC7t');
             await new Promise((resolve, reject) => {
                 var totalHeight = 0;
                 var distance = 100;
                 var timer = setInterval(() => {
-                    const container = document.querySelector('.style-scope ytd-app');
-                    page.focus('.style-scope ytd-app');
+                    //const container = document.querySelector('.m6QErb DxyBCb kA9KIf dS8AEf ecceSd');
                     var scrollHeight = document.body.scrollHeight;
                     window.scrollBy(0, distance);
                     totalHeight += distance;
