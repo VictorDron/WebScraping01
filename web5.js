39d4f2a3b5bde19f915583717aeff98ac81ea22a
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2023-01-08 20:21:47 -0300

    ok

--- web5.js
+++ web5.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
         headless: false
     });
     const page = await browser.newPage();
-    await page.goto('https://www.youtube.com/watch?v=fx7JqqCw1qg');
+    await page.goto('https://www.google.com.br/maps/search/Restaurantes/@-22.9055752,-43.5648952,16z/data=!4m9!2m8!3m6!1sRestaurantes!2sCampo+Grande,+Rio+de+Janeiro+-+RJ!3s0x9be17999363715:0x46c3f27867ad9332!4m2!1d-43.5659121!2d-22.9076515!6e5?hl=pt-BR&authuser=0');
 
     
     await page.setViewport({
@@ -30,9 +30,21 @@ async function autoScroll(page){
     while (true) {
 
         await page.evaluate(async () => {
+            await new Promise((resolve, reject) => {
+                var totalHeight = 0;
+                var distance = 100;
+                var timer = setInterval(() => {
+                    const element = document.querySelector('[class="m6QErb DxyBCb kA9KIf dS8AEf ecceSd"]')[1];
+                    var scrollHeight = element.scrollHeight;
+                    element.scroll(0, distance);
+                    totalHeight += distance;
 
-            const element =  document.querySelector('.m6QErb DxyBCb kA9KIf dS8AEf ecceSd');
-            window.scrollBy(0,100);
+                    if(totalHeight >= scrollHeight){
+                        clearInterval(timer);
+                        resolve();
+                    }
+                }, 100);
+            });
         });
 
     }
