66b50527c906228ebcb3083ef1228efb783aad49
Victor Cesar Ferreira <99146095+VictorDron@users.noreply.github.com>
2023-01-14 22:45:51 -0300

    ok

--- web5.js
+++ web5.js
@@ -1,52 +1,31 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({
-        headless: false
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.goto('https://www.google.com/maps');
+
+  // Digite a pesquisa
+  await page.type('#searchboxinput', 'restaurants near me');
+  
+  // clique no botão de pesquisa
+  await page.click('#searchbox-searchbutton');
+
+  // Aguarde até que os resultados da pesquisa estejam carregados
+  await page.waitForSelector('.m6QErb DxyBCb kA9KIf dS8AEf ecceSd QjC7t');
+
+  // Extraia as informações dos resultados da pesquisa
+  const searchResults = await page.evaluate(() => {
+    const results = Array.from(document.querySelectorAll('.m6QErb DxyBCb kA9KIf dS8AEf ecceSd QjC7t'));
+    return results.map(result => {
+      const title = result.querySelector('.section-result-title span').textContent;
+      const address = result.querySelector('.section-result-location span').textContent;
+      const rating = result.querySelector('.section-result-rating span').textContent;
+      return { title, address, rating };
     });
-    const page = await browser.newPage();
-    await page.goto('https://www.google.com.br/maps/search/Restaurantes/@-22.9055752,-43.5648952,16z/data=!4m9!2m8!3m6!1sRestaurantes!2sCampo+Grande,+Rio+de+Janeiro+-+RJ!3s0x9be17999363715:0x46c3f27867ad9332!4m2!1d-43.5659121!2d-22.9076515!6e5?hl=pt-BR&authuser=0');
+  });
 
-    
-    await page.setViewport({
-        width: 1900,
-        height: 1300
-    });
-    
+  console.log(searchResults);
 
-
-    await autoScroll(page);
-
-    await page.screenshot({
-        path: 'yoursite.png',
-        fullPage: true
-    });
-
-    //await browser.close();
+  await browser.close();
 })();
-
-async function autoScroll(page){
-    
-    while (true) {
-
-        await page.evaluate(async () => {
-            await new Promise((resolve, reject) => {
-                var totalHeight = 0;
-                var distance = 100;
-                var timer = setInterval(() => {
-                    const element = document.querySelector('[class="m6QErb DxyBCb kA9KIf dS8AEf ecceSd"]')[1];
-                    var scrollHeight = element.scrollHeight;
-                    element.scroll(0, distance);
-                    totalHeight += distance;
-
-                    if(totalHeight >= scrollHeight){
-                        clearInterval(timer);
-                        resolve();
-                    }
-                }, 100);
-            });
-        });
-
-    }
-        
-}
